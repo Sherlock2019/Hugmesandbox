ef0f1c8afc4244d1ad64a90efe5f7074ed3655a8
Dzoan Nguyen <you@example.com>
2025-11-04 17:19:52 +0700

    auto: local update 2025-11-04 17:19:52

--- pushonly.sh
+++ pushonly.sh
@@ -6,6 +6,6 @@ echo "⬆️ Pushing local changes to remote (origin/main)..."
 # Auto-detect new/modified files and commit with timestamp
 git add -A
 git commit -m "auto: local update $(date +'%Y-%m-%d %H:%M:%S')" || echo "ℹ️ Nothing to commit."
-git push origin main
+git push -f  origin main
 
 echo "✅ Local changes pushed successfully."
